aed13c50a7199ff56fc77ee350bc71f0f719712a
minhtoansbo <148229501+minhtoansbo@users.noreply.github.com>
2023-10-19 21:28:56 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -1,10 +1,10 @@
 const CONFIGDATA = {
-  titleHeader: "Chào cậu, tớ có điều muốn nói. </br> Làm người yêu tớ nhé!",
+  titleHeader: "Chào cậu, tớ có điều muốn nói. </br> Cậu có phải cô gái cute nhất thế giới đúng khum?",
   descriptionHeader:
     "Hãy trả lời một cách thật lòng nhé. Nếu cậu im lặng và rời đi thì tức là cậu đã ĐỒNG Ý.",
-  buttonYes: "Tớ đồng ý",
-  buttonNo: "Không đồng ý",
+  buttonYes: "Tớ đồng ý🥰",
+  buttonNo: "Không đồng ý😒",
   titleModar: "Cảm ơn cậu.",
   descriptionModar:
-    "Cảm ơn cậu đã cho tớ một cơ hội để tớ có thể ở bên cạnh, che chở và bảo vệ cậu. </br> Tớ biết tớ không được đẹp trai, cũng chẳng lắm tài nhưng tớ có một trái tim chân thành. Cậu chính là cả thế giới của tớ. </br> Tối nay tớ sẽ đến đón cậu. Cậu mặc thật đẹp vào nhé! Sẽ có một bất ngờ dành cho cậu.</br> Love you <3",
+    "Nhân ngày 20/10, tớ muốn gửi đến cậu những lời chúc ngọt ngào và lãng mạn nhất.</br> Cậu là người phụ nữ tuyệt vời nhất. Cậu xinh đẹp, thông minh, và tốt bụng. Tớ thương cậu hơn tất cả mọi thứ trên đời. </br> Hôm nay là một ngày thận ý nghĩa, cũng hơn nửa năm rùi nhỉ, hãy luôn xinh đẹp, hạnh phúc và thành công trong cuộc sống nhé! </br> Love you <3 🥰🥰🥰",
 };
